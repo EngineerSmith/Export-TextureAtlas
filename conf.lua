ed5cc913b9e52f6dacb03ef539da58a5816206ac
EngineerSmith <pasmith.bus@gmail.com>
2021-11-25 21:04:10 +0000

    Added conf.lua to disable unrequired modules

---
+++ conf.lua
@@ -0,0 +1,21 @@
+love.conf = function(t)
+  t.modules.graphics = true
+  t.modules.window   = true
+  t.modules.image    = true
+  t.modules.event    = true
+
+  t.modules.audio    = false
+  t.modules.data     = false
+  t.modules.font     = false
+  t.modules.joystick = false
+  t.modules.keyboard = false
+  t.modules.math     = false
+  t.modules.mouse    = false
+  t.modules.physics  = false
+  t.modules.sound    = false
+  t.modules.system   = false
+  t.modules.thread   = false
+  t.modules.timer    = false
+  t.modules.touch    = false
+  t.modules.video    = false
+end
